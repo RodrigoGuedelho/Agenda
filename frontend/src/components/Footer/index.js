714a9341d5cb37ca1426e7462d65739a4a1e8f5b
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2020-03-05 01:11:02 -0300

    Criado componente para o footer das paginas;

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import {Card, Button} from 'react-bootstrap'
+
+import './index.css'
+
+
+  export default function Footer() {
+
+    return (
+      <>  
+        <br/>
+       
+        <footer>
+          <Card bg="dark" text="white" >
+          <Card.Header >I love software development :)</Card.Header>
+          <Card.Body>
+            <center>
+              
+              <br />  
+              <small ><b>Email: </b>rodrigohmjj97@hotmail.com</small>
+             
+            </center>
+            
+            <Card.Title >copyright © 2020</Card.Title>
+            
+          </Card.Body>
+          </Card>
+        </footer>
+         
+      </>
+    );
+};
+
+
+
+
+
