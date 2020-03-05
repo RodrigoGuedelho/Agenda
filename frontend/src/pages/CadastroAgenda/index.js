14a69a9cda327144adce0a9684c891858df4b842
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2020-03-05 01:32:12 -0300

    Agora o cadastro de agenda está funcionando;

--- frontend/src/pages/CadastroAgenda/index.js
+++ frontend/src/pages/CadastroAgenda/index.js
@@ -1,28 +1,55 @@
 import React, { Component, Fragment } from 'react';
 
 import { Container, Button, Form, Col, Row } from 'react-bootstrap'
-
+import api from "../../services/api"
 
 export default class CadastroAgenda extends Component {
-
+  state = {
+    nome: "",
+    email: "",
+    telefone: "",
+    github: ""
+  };
+  handleOnSubmit = async e => {
+    e.preventDefault(e);  
+    try {
+      const agenda = await api.post("/agenda/store", 
+        {
+          nome: this.state.nome,
+          email: this.state.email,
+          telefone: this.state.telefone,
+          github: this.state.github
+        }
+      );
+      if (agenda)
+        alert("Cadastro realisado com sucesso.")
+      else  
+        alert("Erro ao tenta efetuar cadastro.")
+      
+    } catch (error) {
+      alert(error);
+    }
+  };
   render() {
     return (
       <Fragment>
         <br />
         <Container>
           <center><h1>Cadastro de pessoa:</h1></center>
-          <Form>
+          <Form onSubmit={this.handleOnSubmit}>
             <Row md={2}>
               <Col>
                 <Form.Group controlId="nome" >
                   <Form.Label>Nome:</Form.Label>
-                  <Form.Control type="text" placeholder="Nome" />
+                  <Form.Control type="text" placeholder="Nome" 
+                    onChange={e => this.setState({ nome: e.target.value })}/>
                 </Form.Group>
                 </Col>  
                 <Col>
                   <Form.Group controlId="email" >
                     <Form.Label>Email:</Form.Label>
-                    <Form.Control type="email" placeholder="Email@Exemplo.com" />
+                    <Form.Control type="email" placeholder="Email@Exemplo.com" 
+                       onChange={e => this.setState({ email: e.target.value })}/>
                   </Form.Group>
                 </Col>  
             </Row>
@@ -31,13 +58,15 @@ export default class CadastroAgenda extends Component {
               <Col sm={3}>
                 <Form.Group controlId="nome" >
                   <Form.Label>Telefone:</Form.Label>
-                  <Form.Control type="text" placeholder="(XX) 9XXXX-XXXX" />
+                  <Form.Control type="text" placeholder="(XX) 9XXXX-XXXX" 
+                     onChange={e => this.setState({ telefone: e.target.value })}/>
                 </Form.Group>
               </Col>  
               <Col sm={9}>
                 <Form.Group controlId="github" >
                   <Form.Label>Github:</Form.Label>
-                  <Form.Control type="text" placeholder="github.com/usuario" />
+                  <Form.Control type="text" placeholder="github.com/usuario" 
+                     onChange={e => this.setState({ github: e.target.value })}/>
                 </Form.Group>
               </Col>  
             </Row>
