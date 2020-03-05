2e242290963597097db27ba76a1fc439c6f4d117
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2020-03-05 00:29:03 -0300

    - Foi configurado cors para que api possa ser acessado pelo frontend;

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import routes from './routes'
+import cors from 'cors'
 import './database'
 
 class App {
@@ -9,7 +10,9 @@ class App {
     this.routes() 
   }
   middlewares () {
+    this.server.use(cors())
     this.server.use(express.json())
+    
   }
 
   routes () {
